2330bfe68e4d4d350694edeff5fca2082aeba28b
Stefan Falkenstein <sjfalken@gmail.com>
2022-02-07 22:02:58 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from urllib.error import ContentTooShortError
+import freesound
+from os import environ
+
+client = freesound.FreesoundClient()
+api_token = environ.get("FREESOUND_TOKEN")
+client.set_token(api_token, auth_type="oauth")
+ 
+# results = client.text_search(query="violin")
+query="drum"
+results = client.text_search(query=query, filter='duration:[0 TO 30] type:wav',)
+
+for sound in results:
+    # sound.retrieve_preview(".",sound.name+".mp3")
+    while True:
+        try:
+            sound.retrieve(f"./sounds/{query}/", name=f"{sound.id}.wav")
+            print(sound.name)
+            break
+        except ContentTooShortError:
+            print("got download error, trying again")
